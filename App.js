e21126b0e1b8c64d7088d5b2f5aaa5eef5e81238
pauline <pauline.feytel@gmail.com>
2020-02-14 09:26:11 +0100

    Cleanup code

--- App.js
+++ App.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import { Container } from './App.styles';
-import Header from './components/Header';
-import RecipesList from './components/RecipesList/RecipesList';
 import RecipesNavigator from './navigation/RecipesNavigator';
 
 export default App = () => {
